c608b897c0975d09714fe6c5faa9358a0f503ebf
Nridha0 <ridha.noor@yahoo.com>
2022-07-27 22:47:23 -0400

    still working on it

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+--Create db
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+USE employeeTracker_db;
+
+--create department table
+CREATE TABLE department(
+id INT NOT NULL auto_increment PRIMARY KEY,
+name VARCHAR(30)
+);
+
+--create role table
+CREATE TABLE role_t (
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL, 
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+--create employees table
+CREATE TABLE employees(
+    id INT PRIMARY KEY auto_increment NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_t_id) REFERENCES role_t(id),
+);
+
+SELECT * FROM employees;
+SELECT * FROM department;
+SELECT * FROM role_t;
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department(name)
+VALUES 
+('Software Engineer'),
+('Sales'),
+('CEO'),
+('Finance');
+
+INSERT INTO role_t (title, Salary, department_id)
+('Full Stack Developer', 80000, 1),
+('HR Conslutant', 36000, 4),
+('Accountant', 125000, 3),
+('Sales Lead', 100000, 3),
